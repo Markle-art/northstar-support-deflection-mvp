7841a5cc99bb5f22c6e5105a4754114327b62065
Lekayia Mark <lekayiamark@gmail.com>
2026-08-20 05:35:03 +0300

    Add HMAC signature generation for payloadCalculate HMAC SHA256 signature

--- prototypes/webhook-verification/webhook_verification.py
+++ prototypes/webhook-verification/webhook_verification.py
@@ -2,3 +2,10 @@ import hmac
 import hashlib
 secret = b"It's a Secret to Everybody"
 payload = b"Hello, World!"
+signature = hmac.new(
+    secret,
+    payload,
+    hashlib.sha256
+).hexdigest()
+
+print(signature)
